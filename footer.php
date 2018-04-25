188e59fae4f6f05c0cb30280b4ddc43f719478b5
Amy Singleton <developer@psycharmor.org>
2018-04-25 16:09:43 -0700

    Fixed HTMl Error missing ending tag for page div in footer.php

--- footer.php
+++ footer.php
@@ -25,18 +25,17 @@
 		</div><!-- footer-widgets-->
 	</footer><!-- #colophon -->
 	<div class="col-sm-12 below-footer-container">
-	<div id="copyright" class="col-sm-5">
-		<p>Copyright &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'title' ); ?></p>
-	</div><!-- #copyright -->
-	<div id="guidestar" class="col-sm-2">
-		<p><img class="aligncenter" src="<?php bloginfo( 'url' ); ?>/wp-content/themes/learnarmor-child/css/images/guidestar-platinum-participant.png" alt="guidstar platinum nonprofit participant" /></p>
-	</div><!-- #guidestar -->
-	<div id="legal-nav" class="col-sm-5">
+		<div id="copyright" class="col-sm-5">
+			<p>Copyright &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'title' ); ?></p>
+		</div><!-- #copyright -->
+		<div id="guidestar" class="col-sm-2">
+			<p><img class="aligncenter" src="<?php bloginfo( 'url' ); ?>/wp-content/themes/learnarmor-child/css/images/guidestar-platinum-participant.png" alt="guidstar platinum nonprofit participant" /></p>
+		</div><!-- #guidestar -->
+		<div id="legal-nav" class="col-sm-5">
 		<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-links', 'menu_class' => '') ); ?>
 	</div><!-- #legal-nav -->
-</div><!-- #page -->
-
+	</div><!-- .below-footer-container -->
 <?php wp_footer(); ?>
-
+</div><!-- #page -->
 </body>
 </html>
