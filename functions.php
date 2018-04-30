29917d76e7aa5e5b28359dd7efa87264058a3bb5
Amy Singleton <developer@psycharmor.org>
2018-04-30 15:05:21 -0700

    Added aria label to home page banner search field

--- functions.php
+++ functions.php
@@ -288,8 +288,8 @@ function add_login_logout_link($items, $args) {
 function learnarmor_child_searchform( $form ) {
  
     $form = '<div class="col-sm-12"><form role="search" method="get" id="searchform" class="col-sm-6" action="' . home_url( '/' ) . '" >
-    <label class="screen-reader-text" for="s">' . __('Search for:') . '</label>
-    <input type="text" value="' . get_search_query() . '" name="s" id="search-field" class="col-sm-8" placeholder="What would you like to learn about?"/>
+    <label id="s" class="screen-reader-text" for="s">' . __('Search for:') . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="search-field" class="col-sm-8" placeholder="What would you like to learn about?" aria-labelledby="searchform s" />
     <input type="submit" id="search-submit-banner" class="col-sm-3" value="'. esc_attr__('Search') .'" />
     </form></div>';
  
