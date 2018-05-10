0d85102fdf47cdfbe5edd81432be97c84d85c707
Amy Singleton <developer@psycharmor.org>
2018-05-10 15:58:31 -0700

    Formatted the file

--- learndash/course.php
+++ learndash/course.php
@@ -51,7 +51,6 @@
 		<br />
 	</span>
 	<br />
-
 	<?php
 		/**
 		 * Filter to add custom content after the Course Status section of the Course template output.
@@ -61,7 +60,6 @@
 		 */
 		echo apply_filters( 'ld_after_course_status_template_container', '', learndash_course_status_idx( $course_status ), $course_id, $user_id );
 	?>
-
 	<?php if ( ! empty( $course_certficate_link ) ) : ?>
 		<div id="learndash_course_certificate" class="learndash_course_certificate">
 			<a href='<?php echo esc_attr( $course_certficate_link ); ?>' class="btn-blue" target="_blank"><?php echo apply_filters( 'ld_certificate_link_label', esc_html__( 'PRINT YOUR CERTIFICATE', 'learndash' ), $user_id, $post->ID ); ?></a>
@@ -69,9 +67,7 @@
 		<br />
 	<?php endif; ?>
 <?php endif; ?>
-
 <div class="learndash_content"><?php echo $content; ?></div>
-
 <?php if ( ! $has_access ) : 
 	if(!is_user_logged_in()){
 		?>
@@ -82,7 +78,6 @@
 	echo learndash_payment_buttons( $post );
 } ?>
 <?php endif; ?>
-
 <?php if ( ( isset( $materials ) ) && ( ! empty( $materials ) ) ) : ?>
 	<div id="learndash_course_materials" class="learndash_course_materials">
 		<h4>
@@ -94,16 +89,14 @@
 		<p><?php echo $materials; ?></p>
 	</div>
 <?php endif; ?>
-
 <?php if ( $has_course_content ) : ?>
 	<?php
 		$show_course_content = true;
 	if ( ! $has_access ) :
 		if ( 'on' === $course_meta['sfwd-courses_course_disable_content_table'] ) :
 			$show_course_content = false;
-			endif;
 		endif;
-
+	endif;
 	if ( $show_course_content ) :
 		?>
 	<div id="learndash_course_content" class="learndash_course_content">
@@ -113,7 +106,6 @@
 		 */
 		?>
 		<?php if ( ! empty( $lessons ) ) : ?>
-
 			<?php if ( $has_topics ) : ?>
 				<div class="expand_collapse">
 					<a href="#" onClick='jQuery("#learndash_post_<?php echo $course_id; ?> .learndash_topic_dots").slideDown(); return false;'><?php esc_html_e( 'Expand All', 'learndash' ); ?></a> | <a href="#" onClick='jQuery("#learndash_post_<?php echo esc_attr( $course_id ); ?> .learndash_topic_dots").slideUp(); return false;'><?php esc_html_e( 'Collapse All', 'learndash' ); ?></a>
@@ -126,91 +118,85 @@
 					</script>	
 				<?php } ?>
 			<?php endif; ?>
-
 			<div id="learndash_lessons" class="learndash_lessons">
 				<div id="lesson_heading">
-						<div class="col-sm-6"><?php echo LearnDash_Custom_Label::get_label( 'lessons' ); ?></div>
-					<div class="colo-sm-6 text-align-right status"><?php esc_html_e( 'Status', 'learndash' ); ?></div>
-
+					<div class="col-sm-6">
+						<?php echo LearnDash_Custom_Label::get_label( 'lessons' ); ?>
+					</div>
+					<div class="colo-sm-6 text-align-right status">
+						<?php esc_html_e( 'Status', 'learndash' ); ?>
+					</div>
 				<div id="lessons_list" class="lessons_list">
-				
-					<?php foreach ( $lessons as $lesson ) : ?>
-						<div class="col-sm-12 post-<?php echo esc_attr( $lesson['post']->ID ); ?> <?php echo esc_attr( $lesson['sample'] ); ?>">
-
-							<div class="col-sm-1">
-								<?php echo $lesson['sno']; ?>
-							</div>
-
-							<span class="col-sm-11 course-lesson-link">
-								<a class='col-sm-11 <?php echo esc_attr( $lesson['status'] ); ?>' href='<?php echo esc_attr( learndash_get_step_permalink( $lesson['post']->ID, $course_id ) ); ?>'><?php echo $lesson['post']->post_title; ?></a>
-								<span class="<?php echo esc_attr( $lesson['status'] ); ?> col-sm-1 glyphicon" aria-hidden="true"></span>
-							<div class="lesson-info col-sm-11">
-								<hr>
-								<p>
-									<span class="dashicons dashicons-video-alt3"></span>
-									<span class="bold-font"></span>Video<span class="dashicons dashicons-clock"></span>Duration
-										<?php
-											$value = 'learnarmor_duration_video_duration';
-											echo get_post_meta( $lesson['post']->ID, $value, true );
-										?>
-								</p>
-							</div>
-							<div class="lesson-info col-sm-1">
-							</div>
+				<?php foreach ( $lessons as $lesson ) : ?>
+					<div class="col-sm-12 post-<?php echo esc_attr( $lesson['post']->ID ); ?> <?php echo esc_attr( $lesson['sample'] ); ?>">
+						<div class="col-sm-1">
+							<?php echo $lesson['sno']; ?>
+						</div>
+						<span class="col-sm-11 course-lesson-link">
+							<a class='col-sm-11 <?php echo esc_attr( $lesson['status'] ); ?>' href='<?php echo esc_attr( learndash_get_step_permalink( $lesson['post']->ID, $course_id ) ); ?>'><?php echo $lesson['post']->post_title; ?></a>
+							<span class="<?php echo esc_attr( $lesson['status'] ); ?> col-sm-1 glyphicon" aria-hidden="true"></span>
+							<?php
+												
+								$value = 'learnarmor_duration_video_duration';
+								if (!empty (get_post_meta( $lesson['post']->ID, $value, true ))){
+									echo '<div class="lesson-info col-sm-11">';
+									echo '<hr>';
+									echo '<p>';
+									echo '<span class="dashicons dashicons-video-alt3"></span>';
+									echo 'Video' . ' ' . '<span class="dashicons dashicons-clock"></span>';
+									echo 'Duration' . ' ' . get_post_meta( $lesson['post']->ID, $value, true );
+									echo '</p>';
+									echo '</div>';
+									echo '<div class="lesson-info col-sm-1"></div>';
+								}
+							/**
+							 * Not available message for drip feeding lessons
+							 */
+							?>
+							<?php if ( ! empty( $lesson['lesson_access_from'] ) ) : ?>
 								<?php
-								/**
-								 * Not available message for drip feeding lessons
-								 */
+									SFWD_LMS::get_template(
+										'learndash_course_lesson_not_available',
+										array(
+											'user_id' => $user_id,
+											'course_id' => learndash_get_course_id( $lesson['post']->ID ),
+											'lesson_id' => $lesson['post']->ID,
+											'lesson_access_from_int' => $lesson['lesson_access_from'],
+											'lesson_access_from_date' => learndash_adjust_date_time_display( $lesson['lesson_access_from'] ),
+											'context' => 'course',
+										), true
+									);
 								?>
-								<?php if ( ! empty( $lesson['lesson_access_from'] ) ) : ?>
-									<?php
-										SFWD_LMS::get_template(
-											'learndash_course_lesson_not_available',
-											array(
-												'user_id' => $user_id,
-												'course_id' => learndash_get_course_id( $lesson['post']->ID ),
-												'lesson_id' => $lesson['post']->ID,
-												'lesson_access_from_int' => $lesson['lesson_access_from'],
-												'lesson_access_from_date' => learndash_adjust_date_time_display( $lesson['lesson_access_from'] ),
-												'context' => 'course',
-											), true
-										);
-									?>
-								<?php endif; ?>
+							<?php endif; ?>
+							<?php
+							/**
+							 * Lesson Topics
+							 */
+							?>
+							<?php $topics = @$lesson_topics[ $lesson['post']->ID ]; ?>
+							<?php if ( ! empty( $topics ) ) : ?>
+								<div id='learndash_topic_dots-<?php echo esc_attr( $lesson['post']->ID ); ?>' class="learndash_topic_dots type-list">
+									<ul>
+										<?php $odd_class = ''; ?>
+										<?php foreach ( $topics as $key => $topic ) : ?>
+											<?php $odd_class       = empty( $odd_class ) ? 'nth-of-type-odd' : ''; ?>
+											<?php $completed_class = empty( $topic->completed ) ? 'topic-notcompleted' : 'topic-completed'; ?>												
+											<li class='<?php echo esc_attr( $odd_class ); ?>'>
+												<span class="topic_item col-sm-12">
+													<a class='topic-lesson-link <?php echo esc_attr( $completed_class ); ?>' href='<?php echo esc_attr( learndash_get_step_permalink( $topic->ID, $course_id ) ); ?>' title='<?php echo esc_attr( $topic->post_title ); ?>'>
+														<span class="col-sm-11"><?php echo $topic->post_title; ?></span>
+													</a>
+												</span>
+												<span class="<?php echo esc_attr( $completed_class ); ?> col-sm-1 glyphicon glyphicon-unchecked" aria-hidden="true"></span>
+											</li>
+										<?php endforeach; ?>
+									</ul>
+								</div>
+							<?php endif; ?>
 
-
-								<?php
-								/**
-								 * Lesson Topics
-								 */
-								?>
-								<?php $topics = @$lesson_topics[ $lesson['post']->ID ]; ?>
-
-								<?php if ( ! empty( $topics ) ) : ?>
-									<div id='learndash_topic_dots-<?php echo esc_attr( $lesson['post']->ID ); ?>' class="learndash_topic_dots type-list">
-										<ul>
-											<?php $odd_class = ''; ?>
-											<?php foreach ( $topics as $key => $topic ) : ?>
-												<?php $odd_class       = empty( $odd_class ) ? 'nth-of-type-odd' : ''; ?>
-												<?php $completed_class = empty( $topic->completed ) ? 'topic-notcompleted' : 'topic-completed'; ?>												
-												<li class='<?php echo esc_attr( $odd_class ); ?>'>
-													<span class="topic_item col-sm-12">
-														<a class='topic-lesson-link <?php echo esc_attr( $completed_class ); ?>' href='<?php echo esc_attr( learndash_get_step_permalink( $topic->ID, $course_id ) ); ?>' title='<?php echo esc_attr( $topic->post_title ); ?>'>
-															<span class="col-sm-11"><?php echo $topic->post_title; ?></span>
-														</a>
-													</span>
-													<span class="<?php echo esc_attr( $completed_class ); ?> col-sm-1 glyphicon glyphicon-unchecked" aria-hidden="true"></span>
-												</li>
-											<?php endforeach; ?>
-										</ul>
-									</div>
-								<?php endif; ?>
-
-							</span>
-						
-			</div>
-					<?php endforeach; ?>
-
+						</span>
+					</div>
+				<?php endforeach; ?>
 				</div>
 			</div>
 			<?php
@@ -226,7 +212,6 @@
 				}
 			?>
 		<?php endif; ?>
-		
 		<?php
 			if ( ( isset( $course_lessons_results['pager'] ) ) && ( !empty( $course_lessons_results['pager'] ) ) ) {
 				if ( $course_lessons_results['pager']['paged'] == $course_lessons_results['pager']['total_pages'] ) {
@@ -252,7 +237,6 @@
 								<div class="col-sm-6 text-align-right status"><?php esc_html_e( 'Status', 'learndash' ); ?></span>
 						</div>
 						<div id="quiz_list" class="quiz_list">
-
 							<?php foreach ( $quizzes as $quiz ) : ?>
 								<div id='post-<?php echo esc_attr( $quiz['post']->ID ); ?>' class='col-sm-12 <?php echo esc_attr( $quiz['sample'] ); ?>'>
 									<div class="list-count col-sm-1"><?php echo $quiz['sno']; ?></div>
@@ -262,7 +246,6 @@
 									</span>
 								</div>						
 							<?php endforeach; ?>
-
 						</div>
 					</div>
 				<?php }
