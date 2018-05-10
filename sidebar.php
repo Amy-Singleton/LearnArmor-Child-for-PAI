0f737e23cd9f6136ce29368ba5eb823ac1ec96dd
Amy Singleton <developer@psycharmor.org>
2018-05-10 15:23:33 -0700

    Fixed ending tag for #secondary in sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -14,4 +14,4 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <div id="secondary" class="widget-area col-sm-3" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+</div><!-- #secondary -->
