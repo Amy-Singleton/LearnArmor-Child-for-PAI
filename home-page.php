7405be742d30a4cfd6bee934e55eac59ed1aa9d7
Amy Singleton <developer@psycharmor.org>
2018-05-10 14:59:34 -0700

    Removed comments from home-page.php front page template

---
+++ home-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * This is a full width template that does not have a sidebar
+ * 
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package learnarmor
+ */
+
+get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main col-sm-12" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'no-title' ); ?>
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
