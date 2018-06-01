4fde5d02218fca4329951b0d73ed2a7e4086f3f4
Amy Singleton <developer@psycharmor.org>
2018-06-01 09:46:15 -0700

    Removed code for comments

--- full-width-page.php
+++ full-width-page.php
@@ -8,23 +8,11 @@
  *
  * @package learnarmor
  */
-
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main col-sm-12" role="main">
-
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<div class="col-sm-12">
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
-				</div>
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
