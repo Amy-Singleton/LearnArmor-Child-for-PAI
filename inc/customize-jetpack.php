d5770565aa8757d8eb84057d79adeea3b87217b4
Amy Singleton <developer@psycharmor.org>
2018-06-04 08:34:31 -0700

    Added a function to exclude IVMF, UNCW and Phoenix related content

--- inc/customize-jetpack.php
+++ inc/customize-jetpack.php
@@ -6,7 +6,7 @@
  */
 function jetpackme_filter_exclude_category( $filters ) {
     $filters[] = array( 'not' =>
-      array( 'term' => array( 'taxonomy.ld_course_category.slug' => 'branded' ) )
+      array( 'term' => array( 'taxonomy.ld_courses_category.slug' => 'branded' ) )
     );
     return $filters;
 }
@@ -84,3 +84,57 @@ function learnarmor_child_remove_jetpackme_responsive_videos_setup() {
 }
 add_action( 'after_setup_theme', 'learnarmor_child_remove_jetpackme_responsive_videos_setup', 1 );
 
+
+function learnarmor_child_jetpackme_exclude_related_post( $exclude_post_ids, $post_id ) {
+    // $post_id is the post we are currently getting related posts for
+    if ( is_singular() == '15-things-veterans-want-know-uncw' || is_singular() == '15-things-veterans-want-know-ivmf' || is_singular() == '15-things-veterans-want-know-phoenix') {
+        $exclude_post_ids[] = 15637; // 15 Things for HCP
+        $exclude_post_ids[] = 7969; // 15 Things
+    }
+    if ( is_singular() == '15-things-veterans-want-know') {
+        $exclude_post_ids[] = 15637; // 15 Things for HCP
+    }
+     if ( is_singular() == '15-things-veterans-want-know-healthcare-providers') {
+        $exclude_post_ids[] = 7969; // 15 Things
+    }
+    $exclude_post_ids[] = 10209;    // UNCW
+    $exclude_post_ids[] = 10141;    // UNCW
+    $exclude_post_ids[] = 10254;    // UNCW
+    $exclude_post_ids[] = 10314;    // UNCW
+    $exclude_post_ids[] = 10261;    // UNCW
+    $exclude_post_ids[] = 10322;    // UNCW
+    $exclude_post_ids[] = 10332;    // UNCW
+    $exclude_post_ids[] = 10338;    // UNCW
+    $exclude_post_ids[] = 10268;    // UNCW
+    $exclude_post_ids[] = 10273;    // UNCW
+    $exclude_post_ids[] = 10342;    // UNCW
+    $exclude_post_ids[] = 10280;    // UNCW    
+    $exclude_post_ids[] = 10118;    // IVMF
+    $exclude_post_ids[] = 10119;    // IVMF
+    $exclude_post_ids[] = 10120;    // IVMF
+    $exclude_post_ids[] = 10121;    // IVMF
+    $exclude_post_ids[] = 10122;    // IVMF
+    $exclude_post_ids[] = 10123;    // IVMF
+    $exclude_post_ids[] = 10124;    // IVMF
+    $exclude_post_ids[] = 10125;    // IVMF
+    $exclude_post_ids[] = 10126;    // IVMF
+    $exclude_post_ids[] = 10069;    // IVMF
+    $exclude_post_ids[] = 9954;     // IVMF
+    $exclude_post_ids[] = 50789;    // Phoenix
+    $exclude_post_ids[] = 50791;    // Phoenix
+    $exclude_post_ids[] = 50795;    // Phoenix
+    $exclude_post_ids[] = 50797;    // Phoenix
+    $exclude_post_ids[] = 50793;    // Phoenix
+    $exclude_post_ids[] = 50799;    // Phoenix
+    return $exclude_post_ids;
+}
+add_filter( 'jetpack_relatedposts_filter_exclude_post_ids', 'learnarmor_child_jetpackme_exclude_related_post', 20, 2 );
+
+// change thumbnail size
+function jetpackchange_image_size ( $thumbnail_size ) {
+ $thumbnail_size['width'] = 326;
+ $thumbnail_size['height'] = 245;
+// $thumbnail_size['crop'] = true;
+ return $thumbnail_size;
+}
+add_filter( 'jetpack_relatedposts_filter_thumbnail_size', 'jetpackchange_image_size' );
