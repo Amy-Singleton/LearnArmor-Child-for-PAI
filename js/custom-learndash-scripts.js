5f348dbdb7860c2e334cc2b9d8344c9df2110ba5
Amy Singleton <developer@psycharmor.org>
2018-06-04 09:37:34 -0700

    Added a condition to change the column classes for course description and course image conditionally

--- js/custom-learndash-scripts.js
+++ js/custom-learndash-scripts.js
@@ -6,6 +6,12 @@ jQuery(document).ready(function($){
         $('span.topic-completed.glyphicon').removeClass('glyphicon-unchecked');
         $('span.topic-completed.glyphicon' ).addClass('glyphicon-check');
         $('span.topic-notcompleted.glyphicon').addClass('glyphicon-unchecked');
-        $('span.topic-notcompleted.glyphicon' ).removeClass('glyphicon-check');     
+        $('span.topic-notcompleted.glyphicon' ).removeClass('glyphicon-check');
+        if (screen.width < 1440) {
+                $('div.course-description > div.course-thumbnail' ).removeClass('col-sm-4');
+                $('div.course-description > div.course-thumbnail' ).addClass('col-sm-6');
+                $('div.course-description > div.course-excerpt' ).removeClass('col-sm-8');
+                $('div.course-description > div.course-excerpt' ).addClass('col-sm-6');                
+        }
 });
 
